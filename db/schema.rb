ce352e42a1bee29888a663e1f0ddfacdcc704cdf
Arthur C <constancio_arthur@yahoo.com>
2026-06-01 17:14:22 -0300

    feat: add devise authentication to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_01_194500) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_200556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -45,8 +45,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_01_194500) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "encrypted_password", default: "", null: false
     t.string "name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "articles", "users"
