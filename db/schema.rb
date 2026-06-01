ae0b7768c42487da451b0bc4e98579393392f50c
Arthur C <constancio_arthur@yahoo.com>
2026-06-01 17:38:29 -0300

    feat: associate posts with users and display author on post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_01_200556) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_203013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,7 +39,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_01_200556) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.integer "views", default: 0
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_01_200556) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
