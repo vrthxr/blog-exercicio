f94b9a5428f6556925b87cf2867634454a5c8b52
Arthur C <constancio_arthur@yahoo.com>
2026-06-02 09:38:56 -0300

    fix: correct comments controller and show post form rendering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Comentário criado com sucesso."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Comentário não foi criado."
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
