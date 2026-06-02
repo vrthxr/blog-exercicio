264906c0762eec141d64be5a7826ca441ed7c2f7
Arthur C <constancio_arthur@yahoo.com>
2026-06-02 10:17:17 -0300

    feat: add update action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,17 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def update
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html{redirect_to post_url(@post), notice: "Comentário Atualizado"}
+      else
+        format.html{redirect_to post_url(@post), alert: "Erro na Atualização"}
+      end
+    end
+  end
+
 
   private
 
